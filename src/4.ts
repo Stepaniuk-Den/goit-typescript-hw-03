324eae03699a90d9543f583dc5b7f82cd3863eb6
Stepaniuk-Den <stepaniuk.den@gmail.com>
2023-12-27 12:27:55 +0200

    corrected tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,40 @@
+class Key {
+  private signature: number = Math.floor(Math.random() * 10);
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean;
+  private tenants: Person[] = [];
+
+  constructor(public key: Key) {}
+
+  public comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +43,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
